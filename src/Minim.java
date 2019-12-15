748612deb56bc779e946c9c89401e15b0736691e
Dorin Simion <dorin_simion_1987@yahoo.com>
2019-12-15 11:54:59 +0200

    Initial commit

---
+++ src/Minim.java
@@ -0,0 +1,8 @@
+public class Minim {
+    public static void main(String[] args) {
+        int a =4;
+        int b=5;
+        int minim = a<b ? a : b;
+        System.out.println("Minimul este "+ minim);
+    }
+}
